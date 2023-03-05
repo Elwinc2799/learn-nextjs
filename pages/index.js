ba3dbe5a86f7daa4f409a322fc4d9b9712cc4baf
Elwinc2799 <elwinczh@gmail.com>
2023-03-05 17:18:01 +0800

    update profile picture and self intro

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,9 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>Hello, I'm Elwin. I enjoy problem solving and exploring new
+          technologies in the tech industry. Seeking for any opportunity to grow
+          and learn from experienced individuals.</p>
         <p>
           (This is a sample website - you’ll be building a site like this in{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
